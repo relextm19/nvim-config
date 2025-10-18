b9beea4e6e32cf826a94517710aac40f35b5b5a2
= <=>
2025-10-18 23:13:45 +0200

    added vim global to lua lsp

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -15,14 +15,20 @@ return {
 		"selene.yml",
 		"stylua.toml",
 	},
-	-- settings = {
-	--     Lua = {
-	--         diagnostics = {
-	--             --     disable = { "missing-parameters", "missing-fields" },
-	--         },
-	--     },
-	-- },
-
-	single_file_support = true,
+	settings = {
+	    Lua = {
+            runtime = {
+                version = "LuaJIT",  -- Neovim uses LuaJIT
+            },
+            diagnostics = {
+                globals = { "vim" }, -- recognize 'vim' as global
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true), -- include Neovim runtime
+                checkThirdParty = false,
+            },
+        },
+    },
+    single_file_support = true,
 	log_level = vim.lsp.protocol.MessageType.Warning,
 }
--- lua/relextm19/config/options.lua
+++ lua/relextm19/config/options.lua
@@ -1,6 +1,8 @@
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
+vim.o.number = true
+vim.o.relativenumber=true
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
