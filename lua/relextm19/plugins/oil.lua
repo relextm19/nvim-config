25863bfbc2019698a6e1b59ea07ff1e1a56d2685
= <=>
2025-10-21 13:48:14 +0200

    now actually showing dotfiles

--- lua/relextm19/plugins/oil.lua
+++ lua/relextm19/plugins/oil.lua
@@ -1,15 +1,17 @@
 return {
-	{
-		'stevearc/oil.nvim',
-		---@module 'oil'
-		---@type oil.SetupOpts
-		opts = {
-            show_hidden = true
+    {
+        "stevearc/oil.nvim",
+        opts = {
+            show_hidden = true,
+
+            view_options = {
+                show_hidden = true,  -- allow dotfiles to appear
+                is_hidden_file = function(name, bufnr)
+                    return false    -- don’t hide any file, including .gitignore
+                end,
+            },
         },
-		-- Optional dependencies
-		dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-		lazy = false,
-	}
+        dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+        lazy = false,
+    }
 }
